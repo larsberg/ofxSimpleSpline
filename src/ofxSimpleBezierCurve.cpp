32b4bd288cc59e782149b814f9bc6901e6aa6ad2
lars berg <laserstorm@gmail.com>
2013-10-24 11:34:45 -0500

    added comments

--- src/ofxSimpleBezierCurve.cpp
+++ src/ofxSimpleBezierCurve.cpp
@@ -1,25 +1,46 @@
 //
 //  ofxSimpleBezierCurve.cpp
 //
-//  Created by laserstorm on 10/23/13.
+//  Created by lars berg on 10/23/13.
 //
 //
 
 #include "ofxSimpleBezierCurve.h"
 
-float ofxSimpleBezierCurve::interpolate( float p0, float p1, float p2, float p3, float t, float t2, float t3 )
+/**
+ * used to 1D interpolation
+ * @param  p0 
+ * @param  p1 
+ * @param  p2 
+ * @param  p3 
+ * @param  w  weight 1
+ * @param  w2 weight 2
+ * @param  w3 weight 3
+ * @return    interpolated float
+ */
+float ofxSimpleBezierCurve::interpolate( float p0, float p1, float p2, float p3, float w, float w2, float w3 )
 {
-	float v0 = ( p2 - p0 ) * 0.5,
-	v1 = ( p3 - p1 ) * 0.5;
+	float v0 = ( p2 - p0 ) * 0.5, v1 = ( p3 - p1 ) * 0.5;
 	
-	return ( 2 * ( p1 - p2 ) + v0 + v1 ) * t3 + ( - 3 * ( p1 - p2 ) - 2 * v0 - v1 ) * t2 + v0 * t + p1;
+	return ( 2 * ( p1 - p2 ) + v0 + v1 ) * w3 + ( - 3 * ( p1 - p2 ) - 2 * v0 - v1 ) * w2 + v0 * w + p1;
 };
 
+/**
+ * get a position along the curve 
+ * @param  k sample position. 0-1
+ * @return   ofVec3f
+ */
 ofVec3f ofxSimpleBezierCurve::getPoint(float k)
 {
 	return getPoint( k, *controlVertices );
 }
 
+/**
+ * get position on curve by interoplating some control controlVertices
+ * @param  k   sample position
+ * @param  _cv our control vertices
+ * @return     ofVec3f
+ */
 ofVec3f ofxSimpleBezierCurve::getPoint(float  k, vector<ofVec3f>& _cv )
 {
 	if(_cv.size() == 0)
@@ -44,18 +65,29 @@ ofVec3f ofxSimpleBezierCurve::getPoint(float  k, vector<ofVec3f>& _cv )
 	return ofVec3f(interpolate( pa.x, pb.x, pc.x, pd.x, weight, w2, w3 ), interpolate( pa.y, pb.y, pc.y, pd.y, weight, w2, w3 ), interpolate( pa.z, pb.z, pc.z, pd.z, weight, w2, w3 ));
 }
 
+/**
+ * apppend a control vertex to our control vertices
+ */
 void ofxSimpleBezierCurve::addControlVertex( ofVec3f v )
 {
 	controlVertices = &cv;
 	cv.push_back( v );
 }
 
+/**
+ * append a vector of control vertices to our control vertices
+ * @param _cv vector of control vertices
+ */
 void ofxSimpleBezierCurve::addControlVertices( vector<ofVec3f> _cv )
 {
 	controlVertices = &cv;
 	cv.insert(cv.end(), _cv.begin(), _cv.end());
 }
 
+/**
+ * point our control vertices to an outside vector of control vertices. useful for animating
+ * @param _cv reference to a vector of control vertices
+ */
 void ofxSimpleBezierCurve::setControlVertices( vector<ofVec3f>& _cv )
 {
 	controlVertices = &_cv;
@@ -74,12 +106,17 @@ void ofxSimpleBezierCurve::setControlVertices( vector<ofVec3f>& _cv )
 	}
 }
 
-
+/**
+ * set the number of subdivisions per segment. I haven't checked to see of this is working perfectly
+ */
 void ofxSimpleBezierCurve::setSubdivisions(int subd)
 {
 	subdivisions = max(subd, 1);
 }
 
+/**
+ * update the polylines vertex positions 
+ */
 void ofxSimpleBezierCurve::update()
 {
 	if(controlVertices->size())
@@ -94,11 +131,17 @@ void ofxSimpleBezierCurve::update()
 	}
 }
 
+/**
+ * draws our polyline
+ */
 void ofxSimpleBezierCurve::draw()
 {
 	polyline.draw();
 }
 
+/**
+ * clears it
+ */
 void ofxSimpleBezierCurve::clear()
 {
 	polyline.clear();
